ef16fb263f2205a0467b66bdb3ce5404c8d166ea
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-05-21 21:37:39 +0200

    chore: refactor files

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts({ include: ["src"], copyDtsFiles: true })],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      fileName: "index",
+    plugins: [dts({ include: ["src"], copyDtsFiles: true })],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            formats: ["es"],
+            fileName: "index",
+        },
+        minify: true,
     },
-    minify: true,
-  },
 });
